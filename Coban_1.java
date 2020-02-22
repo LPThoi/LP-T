84aca71919d47ccc06696a42c9b484952cd1ad08
LPThoi <61357760+LPThoi@users.noreply.github.com>
2020-02-22 23:42:10 +0700

    Add files via upload

---
+++ Coban_1.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+public class Coban_1 {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t\t\t----------Cac cau lenh chay chuong trinh co ban---------");
+        System.out.println("- Println: In ra man hinh va xuong dong");
+        System.out.println("- Printf: In ra man hinh nhung khong xuong dong");
+        System.out.println("- Printf: Cau lenh binh thuong , xuong dong giong C/C++");
+        System.out.println("Le Phat Thoi");
+        System.out.println("Age: 18");
+        System.out.println("Job: Student");
+    }
+}
